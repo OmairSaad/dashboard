16ebb93230d4ce54e7647cb6aa2b577eefd5f007
OmairSaad <alamomair724@gmail.com>
2025-05-29 14:34:32 +0530

    unused imports are removed

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,17 +1,14 @@
 import { type FC } from 'react'
 import Header from '../component/Header'
 import { Outlet } from 'react-router-dom'
-import { SearchProvider } from '../Context/SearchContext'
 
 const RootLayout:FC = () => {
   return (
     <>
-      <SearchProvider>
       <Header />
       <main>
         <Outlet />
       </main>
-      </SearchProvider>
     </>
   )
 }
